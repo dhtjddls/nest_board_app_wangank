dd5cd68b92fcbb9f46f6229cd01a80165b57254a
dhtjddls <dhtjddls12@naver.com>
2023-05-04 17:08:02 +0900

    feat: login시 token 발급 완료

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepoistory: UserRepository) {}
+  constructor(
+    private userRepoistory: UserRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepoistory.createUser(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepoistory.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('login failed');
+    }
+  }
 }
--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -4,6 +4,9 @@ import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entitiy';
 import { CreateBoardDto } from './dto/create-board.dto';
 
+// 1. 레포지토리 역할을 하는 서비스를 만들어서 의존성 주입.
+// 2. 커스텀 레포지토리를 만들어서 사용한다.
+
 @CustomRepository(Board)
 export class BoardRepository extends Repository<Board> {
   async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
